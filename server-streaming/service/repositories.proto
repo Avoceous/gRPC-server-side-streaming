d9c2129e03736a60294cc1c156055edf6869b0ce
jlnuxavo <63419938+Avoceous@users.noreply.github.com>
2022-09-23 22:31:56 +0800

    Add files via upload

---
+++ server-streaming/service/repositories.proto
@@ -0,0 +1,26 @@
+// server-streaming/service/repositories.proto
+
+import "users.proto";
+syntax = "proto3";
+
+option go_package = "github.com/username/server-streaming/service",
+
+service Repo {
+  rpc GetRepos (RepoGetRequest) returns (stream RepoGerReply) {}
+}
+
+mesage RepoGetRequest {
+  string id = 2;
+  string creator_id - 1;
+}
+
+message Repository {
+  string id = 1;
+  string name = 2;
+  string url = 3;
+  User owner = 4;
+}
+
+message RepoGetReply {
+  Repository repo = 1;
+}
